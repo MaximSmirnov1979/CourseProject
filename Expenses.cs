28143962a3fd3092c3d3f3e5e86ba23e0b59531a
User <User@DESKTOP-M4U5492>
2022-08-01 22:14:47 +0400

    AddClassExpenses

---
+++ Expenses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceAccounting
+{
+    internal class Expenses
+    {
+        private double value;
+        private DateTime date;
+        private string сategory;
+
+    }
+}
