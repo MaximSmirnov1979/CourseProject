d0efac84e5ffe8c317873c5ce74044dd97a1be68
MaximSmirnov1979 <109621901+MaximSmirnov1979@users.noreply.github.com>
2022-08-19 20:06:56 +0400

    Add files via upload

--- Expenses.cs
+++ Expenses.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace PersonalFinanceAccounting
 {
-    internal class Expenses
+    public class Expenses
     {
-        private double value;
-        private DateTime date;
-        private string сategory;
+        private double value { get; set; }
+        private DateTime date { get; set; }
+        private string сategory { get; set; }
 
+        public Expenses(double value, DateTime date, string сategory)
+        {
+            this.value = value;
+            this.date = date;
+            this.сategory = сategory;
+        }
+        public Expenses() { }
     }
 }
